1b42daca6251b9e9e6a2912d955954f935f58431
Ana Luiza Lamonier <88122547+analu-lamonier@users.noreply.github.com>
2023-11-20 05:03:00 -0300

    Add files via upload

---
+++ ArvoreAVL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caArvoreAVL
+{
+    internal class ArvoreAVL
+    {
+        private NohArvore root;
+
+        public ArvoreAVL()
+        {
+            root = null;
+        }
+
+        public int fatorBalanceamento()
+        {
+            return fatorBalanceamento(root);
+        }
+
+        public int fatorBalanceamento(NohArvore node)
+        {
+            if(node == null)
+            {
+                return 0;
+            }
+            return node.NoEsquerda.Altura - node.NoDireita.Altura;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ NohArvore.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caArvoreAVL
+{
+    internal class NohArvore
+    {
+        private int valor;
+        private int altura;
+        private NohArvore noEsquerda, noDireita;
+
+        public NohArvore() { }
+        public NohArvore(int valor)
+        {
+            this.valor = valor;
+            altura = 1; // Inicializa a altura do nó como 1
+            noEsquerda = noDireita = null;
+        }
+
+        public int Valor { get => valor; set => valor = value; }
+        public int Altura { get => altura; set => altura = value; }
+        internal NohArvore NoEsquerda { get => noEsquerda; set => noEsquerda = value; }
+        internal NohArvore NoDireita { get => noDireita; set => noDireita = value; }
+    }
+}
